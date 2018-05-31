a1b1d6e2172cba7d31bef8d65604e389c202d57f
Pulkit <pulkit.agarwal@sjsu.edu>
2018-05-31 08:05:27 -0700

    Improved Efficiency

--- src/CheckEdit.java
+++ src/CheckEdit.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 public class CheckEdit {
 
 	/*
@@ -8,6 +8,51 @@ public class CheckEdit {
 	 * pale, pIe -> true pales. pale -> true pale. bale -> true pale. bake -> false
 	 */
 	
+	
+	// Let's see if we can do better
+	public static boolean check(String s, String k) {
+		HashMap<Integer, Character> hmap = new HashMap<>();
+		
+		boolean first = false;
+		int count = 0; 
+		int difference = 0;
+		if(s.length() - 1 > k.length() -1) {
+			first = true;
+		}
+		if(first) {
+			char[] c = s.toCharArray();
+			for(int i=0; i< c.length; i++) {
+				hmap.put(i, c[i]);
+			}
+			for(int j=0; j<k.length()-1; j++) {
+				char ch = k.charAt(j);
+				if(hmap.containsValue(ch)) {
+					count++;
+				}
+			}
+			difference = (s.length()-1) - count;
+			if (difference >= 0 && difference < 2)
+				return true;
+			else
+				return false;
+		} else {
+			char[] c = k.toCharArray();
+			for(int i=0; i< c.length; i++) {
+				hmap.put(i, c[i]);
+			}
+			for(int j=0; j<s.length()-1; j++) {
+				char ch = s.charAt(j);
+				if(hmap.containsValue(ch)) {
+					count++;
+				}
+			}
+			difference = (k.length()-1) - count;
+			if (difference >= 0 && difference < 2)
+				return true;
+			else
+				return false;
+		}
+	}
 	
 	// Runtime O(n^2)
 	public static boolean checkEdit(String s, String k) {
@@ -31,6 +76,8 @@ public class CheckEdit {
 
 	public static void main(String[] args) {
 		boolean ans = CheckEdit.checkEdit("pale", "pale");
-		System.out.println(ans);
+		//System.out.println(ans);
+		boolean ans2 = CheckEdit.check("pal", "pale");
+		System.out.println(ans2);
 	}
 }
